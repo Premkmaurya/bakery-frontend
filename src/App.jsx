daab85b9ec3cf15c0fef2f8400e861ac3c76b7fc
Premkmaurya <premkmaurya123456@gmail.com>
2026-01-11 10:01:37 +0530

    remove lenis and gap between products in catalog

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,8 @@
 import React from "react";
 import AppRoutes from "./routes/AppRoutes";
 
-import Lenis from "lenis";
-
 const App = () => {
-  // Initialize Lenis
-  const lenis = new Lenis({
-    autoRaf: true,
-    lerp: 0.5,
-    duration: 1.6,
-    easing: (t) => Math.min(1, 1.01 - Math.pow(2, -10 * t)),
-    smoothWheel: true,
-    syncTouch: true,
-    syncTouchLerp: 0.1,
-  });
+
   return (
     <>
       <AppRoutes />
